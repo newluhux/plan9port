2c70ff2a6203db8f39ebac58e347496bc9333545
Lu Hui <luhux76@gmail.com>
2022-05-16 20:49:09 +0800

    src/cmd/sdm: add stdin support

--- src/cmd/sdm/sdm.c
+++ src/cmd/sdm/sdm.c
@@ -7,17 +7,68 @@
 #define DEFAULT_INFO "/usr/local/plan9/stardict/default.ifo"
 #define DEFAULT_INDEX "/usr/local/plan9/stardict/default.idx"
 
+static char *getword(Biobuf *input) {
+	if (input == nil) {
+		return nil;
+	}
+	int c;
+
+	char word[256];
+	int i;
+
+	i = 0;
+	word[i] ='\0';
+
+loopgetc:
+	while (1) {
+		c = Bgetc(input);
+		if (c == Beof) {
+			return nil;
+		} else if (c == '\n') {
+			break;
+		} else if (c == '\t') {
+			break;
+		} else if (c == ' ') {
+			break;
+		}
+		word[i++] = c;
+	}
+	if (word[i] != '\0' && i < 256) {
+		word[i+1] = '\0';
+	}
+	if (strlen(word) < 1) {
+		goto loopgetc;
+	}
+	char *ret;
+	ret = strdup(word);
+	return ret;
+}
+
 void main(int argc, char *argv[]) {
 	Stardict_dict *dict;
 	Stardict_index *index;
 	void *data;
+	int from_stdin = 0;
+	Biobuf *stdin_bio;
 
 	char *pattern;
-
 	if (argc < 2) {
-		fprint(2,"Usage: %s word ...\n",argv[0]);
-		exits("usage");
+		from_stdin = 1;
 	}
+
+	if (from_stdin == 1) {
+		stdin_bio = Bfdopen(0,OREAD);
+		if (stdin_bio == nil) {
+			fprint(2,"Can't open stdin.\n");
+			exits("io error");
+		}
+	}
+
+	ARGBEGIN {
+	case 'h':
+		fprint(2,"Usage: sdm word ...\n");
+		exits("usage");
+	} ARGEND
 
 	char *dictfile = DEFAULT_DATA;
 	char *indexfile = DEFAULT_INFO;
@@ -43,10 +94,21 @@ void main(int argc, char *argv[]) {
 		exits("io error");
 	}
 
-	int i;
+	while (1) {
+		if (from_stdin == 1) {
+			pattern = getword(stdin_bio);
+			if (pattern == nil) {
+				break;
+			}
+		} else if (*argv) {
+			pattern = *argv++;
+		} else {
+			break;
+		}
 
-	for (i=1;i<argc;i++) {
-		pattern = argv[i];
+		if (!pattern) {
+			break;
+		}
 		Bseek(dict->index,0,0);
 		while ((index = stardict_readindex(dict)) != nil) {
 			if (strlen(index->word) != strlen(pattern)) {
@@ -56,11 +118,15 @@ void main(int argc, char *argv[]) {
 				data = stardict_loaddata(dict,index);
 				if (data != nil) {
 					write(1,data,index->size);
+					write(1,"\n",1);
 					free(data);
 				}
 			}
 skip:
 			stardict_freeindex(index);
+		}
+		if (from_stdin == 1) {
+			free(pattern);
 		}
 	}
 	stardict_close(dict);
