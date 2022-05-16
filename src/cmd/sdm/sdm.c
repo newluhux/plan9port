ce3a7c8cc6dfb862357cf04e2fa8618dff4c5ba6
Lu Hui <luhux76@gmail.com>
2022-05-16 18:00:03 +0800

    src/cmd/sdm: move sdmm into sdm

--- src/cmd/sdm/sdm.c
+++ src/cmd/sdm/sdm.c
@@ -14,33 +14,55 @@ void main(int argc, char *argv[]) {
 
 	char *pattern;
 
-	if (argc != 2) {
-		fprint(2,"Usage: %s word\n",argv[0]);
+	if (argc < 2) {
+		fprint(2,"Usage: %s word ...\n",argv[0]);
 		exits("usage");
 	}
-	pattern = argv[1];
 
-	dict = stardict_open(DEFAULT_INDEX,DEFAULT_DATA,DEFAULT_INFO);
+	char *dictfile = DEFAULT_DATA;
+	char *indexfile = DEFAULT_INFO;
+	char *infofile = DEFAULT_INDEX;
+
+	char *env;
+	env = getenv("stardict_idx");
+	if (env != nil) {
+		indexfile = env;
+	}
+	env = getenv("stardict_ifo");
+	if (env != nil) {
+		infofile = env;
+	}
+	env = getenv("stardict_dict");
+	if (env != nil) {
+		dictfile = env;
+	}
+
+	dict = stardict_open(indexfile,dictfile,infofile);
 	if (dict == nil) {
 		fprint(2,"Can't open file.\n");
 		exits("io error");
 	}
 
-	while ((index = stardict_readindex(dict)) != nil) {
-		if (strlen(index->word) != strlen(pattern)) {
-			goto skip;
-		}
-		if (strcmp(index->word,pattern) == 0) {
-			data = stardict_loaddata(dict,index);
-			if (data != nil) {
-				write(1,data,index->size);
-				free(data);
-			}
-		}
-skip:
-		stardict_freeindex(index);
-	}
+	int i;
 
+	for (i=1;i<argc;i++) {
+		pattern = argv[i];
+		Bseek(dict->index,0,0);
+		while ((index = stardict_readindex(dict)) != nil) {
+			if (strlen(index->word) != strlen(pattern)) {
+				goto skip;
+			}
+			if (strcmp(index->word,pattern) == 0) {
+				data = stardict_loaddata(dict,index);
+				if (data != nil) {
+					write(1,data,index->size);
+					free(data);
+				}
+			}
+skip:
+			stardict_freeindex(index);
+		}
+	}
 	stardict_close(dict);
 	return;
 }
